6a89bef25d8e8df0d6b140dc9256f8013728e911
Tre-veon <120367@swcsd.us>
2023-02-06 14:14:49 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
+//Treveon Aponte
+//Feb 5
 
 
 public class Main {
     private static Scanner input = new Scanner(System.in);
-    private static ArrayList<String>tasks = new ArrayList<>();
 
 
     public static void main(String[] args) {
+        ArrayList<String> tasks = new ArrayList<>();
+        //Creates the menu to pick from
         System.out.println("Please choose an option:");
         System.out.println("(1) Add a task.");
         System.out.println("(2) Remove a task.");
@@ -17,15 +21,55 @@ public class Main {
         System.out.println("(0) Exit.");
 
         int userInput = input.nextInt();
-        if (userInput == 1){
-            addTask();
-        }
+        input.nextLine();
+        //while loop that allows the user to continue to choose options.
+        while (true) {
+            if (userInput == 1) {
+                addTask(tasks);
+            } else if (userInput == 0) {
+                System.exit(0);
+            } else if (userInput == 2) {
+                removeTask(tasks);
+            } else if (userInput == 4) {
+                listTask(tasks);
+            }else if (userInput == 3){
+                updateTask(tasks);
 
+            }
+
+
+            System.out.println("Please make another selection");
+            userInput = input.nextInt();
+            input.nextLine();
+        }
     }
-    private  static void addTask(){
+    //All the following code are the functions for the menu to work. Like to add tasks, delete tasks, change tasks, list tasks and to close it.
+    private  static ArrayList<String> addTask(ArrayList<String> tasks){
         System.out.println("Enter your new task");
         String task = input.nextLine();
+        tasks.add(task);
+        return tasks;
+    }
 
+     static ArrayList<String> removeTask(ArrayList<String> tasks){
+        System.out.println("Enter the task to remove");
+        String rem = input.nextLine();
+        tasks.remove(rem);
+        return tasks;
+    }
+
+     static void listTask(ArrayList<String>tasks){
+        System.out.println(tasks);
+    }
+
+    static ArrayList<String> updateTask(ArrayList<String> tasks){
+        System.out.println("What index would you like to replace?");
+        int ind = input.nextInt();
+        input.nextLine();
+        System.out.println("What would you like to add?");
+        String change = input.nextLine();
+        tasks.set(ind, change);
+        return tasks;
 
     }
 
